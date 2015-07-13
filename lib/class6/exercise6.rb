65b874a51df9536b2f31685b2aa1c31a740a7ab8
mryman <mark.ryman@gmail.com>
2015-07-12 17:00:59 -0700

    outline of plan only, Contingent on correct solution of ex5 before I can proceed

--- lib/class6/exercise6.rb
+++ lib/class6/exercise6.rb
@@ -22,22 +22,46 @@
 require 'yaml'
 
 def database
-  '/replace/me'
+  File.absolute_path(File.dirname(__FILE__) + ('/database.yml'))
 end
 
 def load
-  { replace: 'me' }
+  filename = database
+  read_hash = File.read filename
+  YAML.load read_hash
+end
+
+def save
+  filename = database
+  File.open filename, 'w' do |f|
+    f.write person.to_yaml
+  end
+end
+
+def find(key)
+  person[key]
 end
 
 def update(key, value)
-  key + value # fix me
+  # key + value # fix me
+  result = find(key)
+
+  if result
+    person[key] = value
+  else
+    person.merge(key => value)
+  end
 end
+
+person = load
+puts person
 
 input1, input2 = ARGV
 
 abort 'Usage: exercise5.rb KEY VALUE' unless input1 && input2
 
 update(input1, input2)
+save
 
 puts "Updated 1 key-value pair in #{database}"
 puts ":#{input1} => #{input2.inspect}"
